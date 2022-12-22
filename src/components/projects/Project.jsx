1ea8169ec833dae88a5b819c851320f5e6bad08a
Maxi Cadús <maxicadus172@gmail.com>
2022-12-22 13:53:13 -0300

    Finished mobile

--- src/components/projects/Project.jsx
+++ src/components/projects/Project.jsx
@@ -1,9 +1,14 @@
 import "../../css/projects/project.css";
 
-function Project() {
+function Project({ values }) {
   return (
-    <div className="project">
-      <span>Title</span>
+    <div
+      className="project"
+      style={{ backgroundImage: `url(${values.bg})`, backgroundSize: "cover" }}
+    >
+      <a className="project-link" href={values.href} target="_blank">
+        {values.name}
+      </a>
     </div>
   );
 }
--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,14 +1,35 @@
 import Project from "./Project";
 import "../../css/projects/projects.css";
+import portfolioImg from "../../assets/images/portfolio-web.png";
+import blogImg from "../../assets/images/blog.png";
+import gamefinderImg from "../../assets/images/gamefinder.png";
 
 function Projects() {
   return (
     <div className="projects" id="projects">
       <h2>Projects</h2>
       <div className="projects-container">
-        <Project />
-        <Project />
-        <Project />
+        <Project
+          values={{
+            name: "GameFinder",
+            bg: gamefinderImg,
+            href: "https://github.com/MaximilianoCadus/GameFinder",
+          }}
+        />
+        <Project
+          values={{
+            name: "Blog",
+            bg: blogImg,
+            href: "https://github.com/MaximilianoCadus/Blog",
+          }}
+        />
+        <Project
+          values={{
+            name: "Portfolio",
+            bg: portfolioImg,
+            href: "https://github.com/MaximilianoCadus/maximilianocadus.github.io",
+          }}
+        />
       </div>
     </div>
   );
